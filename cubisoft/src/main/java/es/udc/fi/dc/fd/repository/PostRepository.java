9a1da781830d864be6b61a565478b7ca45e0fa0c
Bran López Pintor <bran.lopezp@udc.es>
2018-10-24 16:20:39 +0200

    finished fusing Post and FollowsPost features

--- cubisoft/src/main/java/es/udc/fi/dc/fd/repository/PostRepository.java
+++ cubisoft/src/main/java/es/udc/fi/dc/fd/repository/PostRepository.java
@@ -37,6 +37,12 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 	 */
 	@Query("SELECT p FROM Post p WHERE p.user=:user")
 	List<Post> findUserPosts(@Param("user") UserProfile user);
+	
+	
+	@Query("SELECT p FROM Post p WHERE p.user "
+			+ "IN (SELECT f.followed_user FROM Follow f WHERE f.user=:user)"
+			+ "OR p.user=:user ORDER BY p.date DESC")
+	List<Post> findFollowsAndUserPosts(@Param("user") UserProfile user);
 
 	/**
 	 * Find one by post id.
